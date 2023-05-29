95e173b4e0d20d30bd121962203efb355262649f
fcutuk <cutuk.faris@gmail.com>
2023-05-29 15:36:52 +0200

    installed stripe package for payment

--- server/run.py
+++ server/run.py
@@ -1,17 +1,17 @@
 from flask import Flask, request, abort, jsonify
 from config import ApplicationConfig
 from app.models.db.shared import server as app_class
-# from app.models.db.booking import Booking
-# from app.models.db.payment import Payment
-# from app.models.db.preplanned_trip import PreplannedTrip
-# from app.models.db.route import Route
-# from app.models.db.schedule import Schedule
-# from app.models.db.service import Service
-# from app.models.db.station import Station
-# from app.models.db.ticket import Ticket
-# from app.models.db.train import Train
-# from app.models.db.travel_card import TravelCard
-# from app.models.db.user import User
+from app.models.db.booking import Booking
+from app.models.db.payment import Payment
+from app.models.db.preplanned_trip import PreplannedTrip
+from app.models.db.route import Route
+from app.models.db.schedule import Schedule
+from app.models.db.service import Service
+from app.models.db.station import Station
+from app.models.db.ticket import Ticket
+from app.models.db.train import Train
+from app.models.db.travel_card import TravelCard
+from app.models.db.user import User
 from app.blueprints.admin.bookingBlueprint import booking
 from app.blueprints.admin.preplanned_tripBlueprint import preplannedTrip
 from app.blueprints.admin.routeBlueprint import route
